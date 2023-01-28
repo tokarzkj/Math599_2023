98272bfdb330da13c711d90074c08dc080c25911
Kris Tokarz <kj.tokarz@gmail.com>
2023-01-28 08:58:13 -0500

    Implement the IDFT method
    The main change in this PR is the creation of the IDFT method.
    This method takes the frequency and tries to recover the signal
    from it. There are currently some rounding errors that occur in
    both DFT and IDFT that may need to be investigated more.
    
    I also included documentation to help indicate what the functions
    do, their params, and their returns.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 
-def cosine_samples(f, N):
-    signal_vector_results = np.empty(N)
+def cosine_samples(f, N) -> np.array:
+    """
+    Calculates N-samples of a specific cosine signal
+    :param f: An integer
+    :param N: A non-zero integer representing how many samples to take
+    :return: N length vector containing samples of a cosine signal
+    """
+    signal_vector_results = np.empty((N, 1))
     for n in range(0, N):
         signal_vector_results[n] = np.cos((2 * np.pi * f * n)/N)
 
@@ -10,21 +16,32 @@ def cosine_samples(f, N):
 
 
 def sin_samples(f, N) -> np.array:
-    signal_vector_results = np.empty(N)
+    """
+    Calculates N samples of a specific sin signal
+    :param f: An integer
+    :param N: A non-zero integer representing how many samples to take
+    :return: N length vector containing samples of a sin signal
+    """
+    signal_vector_results = np.empty((N, 1))
     for n in range(0, N):
         signal_vector_results[n] = np.sin(2 * np.pi * f * n)
 
     return signal_vector_results
 
 
-def perform_dft_transform(signal) -> np.array:
+def dft_transform(signal) -> np.array:
+    """
+    Use the DFT summation technique to transform a signal vector into frequency vector
+    :param signal: N length vector containing samples of a signal
+    :return: An N length vector of frequency samples
+    """
     i = complex(0, 1)
     N = len(signal)
 
     dft_samples = np.empty((N, 1), dtype=np.complex_)
 
     for n in range(0, N):
-        sample_summation = 0
+        sample_summation = np.float64(0)
         for k in range(0, N):
             signal_sample = signal[k]
             sample_summation += signal_sample * np.exp((-2 * np.pi * i * k * n) / N)
@@ -33,13 +50,38 @@ def perform_dft_transform(signal) -> np.array:
     return dft_samples
 
 
+def inverse_dft_transform(frequency) -> np.array:
+    """
+    Uses the inverse DFT summation technique to recover the signal from a frequency.
+    Currently subject to some rounding errors when comparing to the original signal values.
+    :param frequency: N length vector containing samples of a frequency
+    :return: An N length vector of signal samples
+    """
+    i = complex(0, 1)
+    N = len(frequency)
+    signal_samples = np.empty((N, 1), dtype=np.float_)
+
+    for k in range(0, N):
+        frequency_summation = np.float64(0)
+        for n in range(0, N):
+            frequency_sample = frequency[n]
+            frequency_summation += frequency_sample * np.exp((2 * np.pi * i * k * n)/N)
+        signal_samples[k] = np.float64((1/N) * frequency_summation)
+
+    return signal_samples
+
+
 if __name__ == '__main__':
     f = 1
     N = 8
     cos_signal = cosine_samples(f, N)
     sin_signal = sin_samples(f, N)
 
-    cos_frequency = perform_dft_transform(cos_signal)
-    sin_frequency = perform_dft_transform(sin_signal)
+    cos_frequency = dft_transform(cos_signal)
+    sin_frequency = dft_transform(sin_signal)
+
+    idft_cos_signal = inverse_dft_transform(cos_frequency)
+    idft_sin_signal = inverse_dft_transform(sin_frequency)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
